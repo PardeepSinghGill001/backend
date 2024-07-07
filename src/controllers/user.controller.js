61066beb26eade4b2baa41de04751c911ba69c8b
PardeepSinghGill001 <pradeepgill001@gmail.com>
2024-07-07 10:45:20 +0530

    added register controller and bug fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,68 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import {ApiError} from "../utils/ApiError.js";
+import {User} from "../models/user.models.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler( async (req,res) => {
-    res.status(200).json({
-        message:"ok"
-    })
+   //get user details from frontend
+   //validation-not empty
+   //check if user already exists: username,email
+   //check for images, check for avatar
+   //upload them to cloudinary, avatr
+   //create user object - create entry in db
+   //remove password and refresh token field from response
+   //check for user creation //is it a null response or user created actually
+   //return response
+
+   //data can come from json,form, url. The below code handles for json and form
+   const {username,fullname,email,password}=req.body
+   console.log("email",email);
+
+   if(
+    [fullname,email,username,password].some((field) => 
+    field?.trim()==="")
+   ){
+    throw new ApiError(400,"All fields are required")
+   }
+   const existedUser=User.findOne({
+    $or:[{ username },{ email }]//return me the first document where the username and or email matches
+   })
+   //if user exists, i do not want to proceed, i will throw error
+   if(existedUser){
+    throw new ApiError(409,"User with email or username already exists")
+   }
+   const avatarLocalPath=req.files?.avatar[0]?.path;//getting the path to which multer stored the file on our server
+   const coverImageLocalPath=req.files?.coverImage[0]?.path;
+   if(!avatarLocalPath){
+     throw new ApiError(400, "Avatar file is required")
+   }
+
+
+   const avatar=await uploadOnCloudinary(avatarLocalPath)
+   const coverImage=await uploadOnCloudinary(coverImageLocalPath)
+
+   if(!avatar){
+    throw new ApiError(400, "Avatar file is required")
+   }
+
+   const user =await User.create({
+    fullname,
+    avatar:avatar.url,
+    coverImage:coverImage?.url||"",
+    email,
+    password,
+    username:username.toLowerCase()
+   })
+   const createdUser = await User.findById(user._id).select("-password -refreshToken")
+
+   if(!createdUser){
+    throw new ApiError(500,"Something went wrong while registering the user")
+   }
+
+   return res.status(201).json(
+    new ApiResponse(200,createdUser,"User registered successfully")
+   )   
+
 })
 export {registerUser}
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,19 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
+
 const router = Router()
-router.route("/register").post(registerUser)
+router.route("/register").post(
+    upload.fields([
+        {
+            name:"avatar",
+            maxCount:1
+        },
+        {
+            name:"coverImage",
+            maxCount:1
+        }
+    ]),
+    registerUser)
 //  generated url will look somewhat like: http://localhost:8000/users/register
 export default router
