5e81ec730c56528bc55a795f80aa33a21ac0cd76
PardeepSinghGill001 <pradeepgill001@gmail.com>
2024-08-22 10:40:27 +0530

    new updates

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser, loginUser, logoutUser,refreshAccessToken} from "../controllers/user.controllers.js";
+import { registerUser, loginUser, logoutUser,refreshAccessToken, changeCurrentPassword} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middlewares.js";
 
 import {verifyJWT} from "../middlewares/auth.middlewares.js";
@@ -25,4 +25,13 @@ router.route("/register").post(
     router.route("/logout").post(verifyJWT, logoutUser)
 //  generated url will look somewhat like: http://localhost:8000/users/register
     router.route("/refresh-token").post(refreshAccessToken)
+    router.route("/change-password").post(verifyJWT,changeCurrentPassword);
+    router.route("/current-user").get(verifyJWT, getCurrentUser)
+    router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+
+    router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+    router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+
+    router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+    router.route("/history").get(verifyJWT, getWatchHistory)
 export default router
